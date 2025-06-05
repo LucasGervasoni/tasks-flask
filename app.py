95f35942c2f3e98bce42e7da56e38b522a8f1884
Lucas Gervasoni <jaxellucas@gmail.com>
2025-06-05 11:43:23 -0300

    feat: adding delete function

--- app.py
+++ app.py
@@ -44,5 +44,17 @@ def update_task(id):
     task.completed = data['completed']
     return jsonify({"message": "Tarefa atualizada com sucesso"})
 
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if task == None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa excluída com sucesso"})
+
 if __name__ == '__main__':
     app.run(debug=True)
